12f406e789e6fe9bbff77fe34df302560d3d3847
E. Choroba <choroba@matfyz.cz>
2014-06-09 19:45:14 +0200

    work sum added

---
+++ bin/work-sum.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $sum;
+while (<>) {
+    my ($h, $m, $s) = split /:/, (split)[1];
+    $sum += $s
+            + 60 * $m
+            + 3600 * $h;
+}
+
+print 'Hours: ', int($sum / 3600);
+$sum %= 3600;
+
+print ', minutes: ', int($sum / 60);
+$sum %= 60;
+
+print ', seconds: ', $sum, "\n";
+
+=head1 work-sum.pl
+
+Sums the time in the output of work-today.sh.
+
+=cut
+
